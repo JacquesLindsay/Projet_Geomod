7db70f82c916ac7a4c03e65800e3f1b26b269ed7
JacquesLindsay <jacques.lindsay@orange.fr>
2017-11-22 10:55:52 +0100

    Update mainWidget.h

--- mainWidget.h
+++ mainWidget.h
@@ -1,13 +1,83 @@
-#ifndef MAIN_WINDOW_H
-#define MAIN_WINDOW_H
-
-#include <QMainWindow>
 #include <QString>
 #include <QAction>
 #include <QMenu>
 #include <QDir>
 #include <QTimer>
 
+
+
+void playAnimation();
+void stopAnimation();
+
+private:
+// all the widgets should be there 
+ToolsWidget     *_toolsWidget;
+DrawingWidget   *_drawingWidget;
+AnimationWidget *_animationWidget;
+QTimer          *_timer;
+
+protected:
+void closeEvent(QCloseEvent *event);
+
+private slots:
+void newFile();
+void open();
+bool save();
+bool saveAs();
+void settings();
+void copy();
+void paste();
+void selectAll();
+void help();
+void about();
+bool closeAppli();
+void nextFrame();
+void prevFrame();
+void firstFrame();
+void lastFrame();
+void play();
+void stop();
+void timerEvent();
+
+private:
+void createActions();
+void createMenus();
+bool maybeSave();
+void clearAll();
+void setCurrentFile(const QString &fileName);
+
+QString  _currentFile;
+
+QMenu   *_fileMenu;
+QMenu   *_editMenu;
+QMenu   *_helpMenu;
+QMenu   *_animMenu;
+
+QAction *_newAct;
+QAction *_openAct;
+QAction *_saveAct;
+QAction *_saveAsAct;
+QAction *_settingsAct;
+QAction *_exitAct;
+QAction *_copyAct;
+QAction *_pasteAct;
+QAction *_selectAllAct;
+QAction *_helpAct;
+QAction *_aboutAct;
+QAction *_playAct;
+QAction *_stopAct;
+QAction *_nextFrameAct;
+QAction *_prevFrameAct;
+QAction *_firstFrameAct;
+QAction *_lastFrameAct;
+
+static       QDir    _CURRENT_PATH;
+static const QString _DEFAULT_FILE_NAME;
+static const QString _APPLICATION_NAME;
+static const QString _APPLICATION_EXTENSION;
+};
+
+#endif // MAIN_WINDOW_H
 #include "toolsWidget.h"
 #include "drawingWidget.h"
 #include "animationWidget.h"
@@ -19,7 +89,81 @@ class MainWindow : public QMainWindow {
   MainWindow();
   ~MainWindow();
 
-  inline void refresh() {
+  inl
+  
+  void playAnimation();
+  void stopAnimation();
+
+ private:
+  // all the widgets should be there 
+  ToolsWidget     *_toolsWidget;
+  DrawingWidget   *_drawingWidget;
+  AnimationWidget *_animationWidget;
+  QTimer          *_timer;
+
+ protected:
+  void closeEvent(QCloseEvent *event);
+
+  private slots:
+  void newFile();
+  void open();
+  bool save();
+  bool saveAs();
+  void settings();
+  void copy();
+  void paste();
+  void selectAll();
+  void help();
+  void about();
+  bool closeAppli();
+  void nextFrame();
+  void prevFrame();
+  void firstFrame();
+  void lastFrame();
+  void play();
+  void stop();
+  void timerEvent();
+
+ private:
+  void createActions();
+  void createMenus();
+  bool maybeSave();
+  void clearAll();
+  void setCurrentFile(const QString &fileName);
+
+  QString  _currentFile;
+
+  QMenu   *_fileMenu;
+  QMenu   *_editMenu;
+  QMenu   *_helpMenu;
+  QMenu   *_animMenu;
+
+  QAction *_newAct;
+  QAction *_openAct;
+  QAction *_saveAct;
+  QAction *_saveAsAct;
+  QAction *_settingsAct;
+  QAction *_exitAct;
+  QAction *_copyAct;
+  QAction *_pasteAct;
+  QAction *_selectAllAct;
+  QAction *_helpAct;
+  QAction *_aboutAct;
+  QAction *_playAct;
+  QAction *_stopAct;
+  QAction *_nextFrameAct;
+  QAction *_prevFrameAct;
+  QAction *_firstFrameAct;
+  QAction *_lastFrameAct;
+  
+  static       QDir    _CURRENT_PATH;
+  static const QString _DEFAULT_FILE_NAME;
+  static const QString _APPLICATION_NAME;
+  static const QString _APPLICATION_EXTENSION;
+};
+
+#endif // MAIN_WINDOW_H
+ine void refresh() {
     _drawingWidget->refresh();
     _animationWidget->refresh();
   }
@@ -95,6 +239,7 @@ class MainWindow : public QMainWindow {
   QAction *_newAct;
   QAction *_openAct;
   QAction *_saveAct;
+  QAction *_saveAsAct;
   QAction *_settingsAct;
   QAction *_exitAct;
   QAction *_copyAct;
